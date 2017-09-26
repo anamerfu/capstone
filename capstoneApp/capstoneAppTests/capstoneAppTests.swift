9bd327cb88a53afd80f6dff5610fb2ad933c8bed
anamerfu <anamerfu@gmail.com>
2017-09-26 15:19:12 -0400

    Initial commit

---
+++ capstoneApp/capstoneAppTests/capstoneAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  capstoneAppTests.swift
+//  capstoneAppTests
+//
+//  Created by Ana Merfu on 2017-09-26.
+//  Copyright © 2017 Ana Merfu & Laura Douglas. All rights reserved.
+//
+
+import XCTest
+@testable import capstoneApp
+
+class capstoneAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
