27714243abc92c89d85abffb41302475dc3b92ba
anamerfu <anamerfu@gmail.com>
2017-09-27 19:16:23 -0400

    Revert "."
    This reverts commit c91a9387be19d112e5fb6bf066e084a921cc0b3f.

--- capstoneApp/capstoneApp/AppDelegate.swift
+++ capstoneApp/capstoneApp/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
         let gameViewController =  GameViewController()
-    
+        
         gameViewController.view.backgroundColor = UIColor.red
         
         window!.rootViewController = gameViewController
